3f53103a49bc4e1bc9d40cf3726c67a8cd9ad4b6
Euclides do Rosário <euclidesdry@gmail.com>
2024-07-28 21:32:16 +0100

    refactor(composable): moving ShoppingListApp into a separated file

--- app/src/main/java/com/example/myshoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/example/myshoppinglistapp/MainActivity.kt
@@ -4,28 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.myshoppinglistapp.ui.theme.MyShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,40 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
-                    Column(
-                        modifier = Modifier.fillMaxSize().statusBarsPadding().navigationBarsPadding(),
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Button(
-                            onClick = {},
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
-                        ) {
-                            Text("Add Item")
-                        }
-                        LazyColumn(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(16.dp)
-                        ) {
-                            items(sItems) { item ->
-                                ShoppingItem(
-                                    id = item.id,
-                                    name = item.name,
-                                    quantity = item.quantity
-                                )
-                            }
-                        }
-                    }
+                    ShoppingListApp()
                 }
             }
         }
     }
 }
-
-data class ShoppingItem(
-    val id: Int,
-    var name: String,
-    var quantity: String,
-    var isEditing: Boolean = false
-)
